f5139b0b6cf8afab4ddf17e86d275f10b23ca3e1
Christian <christian_d_ol9000@hotmail.com>
2020-11-18 17:10:25 -0300

    terminado

---
+++ ex 2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ex_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nome = "";
+            string senha =  "";
+
+            while(true){
+                Console.WriteLine("digite seu nome:");
+                    nome= Console.ReadLine();
+
+                Console.WriteLine("digite sua senha :");
+                senha = Console.ReadLine();
+
+                if(nome == senha){
+                    Console.WriteLine("você nao pode ter seu nome em sua senha");
+                }else{break;}
+            }
+            Console.WriteLine("obrigado!!! seja bem-vindo " + nome);
+            }
+        }
+    }
+
